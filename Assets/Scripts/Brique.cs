c77ed3051872caffe0445c6da3e9cabe096e3de2
Vincent <vincentboutin.dev@gmail.com>
2021-01-08 20:11:57 +0100

    feat(core): add bricks

---
+++ Assets/Scripts/Brique.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brique : MonoBehaviour
+{
+    [SerializeField]
+    private int health;
+    public int ScoreValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+    }
+}

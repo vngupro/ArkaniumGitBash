74219b8465e38af1b8966fb9cae9fff0b38d70fd
jqntn <jqntn88@gmail.com>
2021-01-13 01:40:05 +0100

    feat(ui): update ui.cs & scenes

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,16 +1,16 @@
-﻿/* UI - One Scene Game Version */
+﻿/* UI - Multiple Scenes Game */
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 class UI : MonoBehaviour
 {
     // Game State
-    public bool isPaused, isMenued;
+    public bool isPaused, isMenued, isSettinged;
     // UI Objects
     // | Layers
-    GameObject Layer_Runtime, Layer_Pause, Layer_Menu;
+    GameObject Layer_Runtime, Layer_Pause, Layer_Menu, Layer_Settings;
     // | Buttons
-    Button Button_Resume, Button_Play, Button_Menu, Button_Quit;
+    Button Button_Resume, Button_Retry, Button_Play, Button_Menu, Button_Quit_P, Button_Quit_M, Button_Settings;
     void Awake()
     {
         // UI Objects
@@ -18,16 +18,23 @@ class UI : MonoBehaviour
         Layer_Runtime = GameObject.Find("Layer_Runtime");
         Layer_Pause = GameObject.Find("Layer_Pause");
         Layer_Menu = GameObject.Find("Layer_Menu");
+        Layer_Settings = GameObject.Find("Layer_Settings");
         // | Buttons
+        Button_Resume = GameObject.Find("Button_Resume").GetComponent<Button>();
+        Button_Retry = GameObject.Find("Button_Retry").GetComponent<Button>();
         Button_Play = GameObject.Find("Button_Play").GetComponent<Button>();
         Button_Menu = GameObject.Find("Button_Menu").GetComponent<Button>();
-        Button_Quit = GameObject.Find("Button_Quit").GetComponent<Button>();
-        Button_Resume = GameObject.Find("Button_Resume").GetComponent<Button>();
+        Button_Quit_P = GameObject.Find("Button_Quit_P").GetComponent<Button>();
+        Button_Quit_M = GameObject.Find("Button_Quit_M").GetComponent<Button>();
+        Button_Settings = GameObject.Find("Button_Settings").GetComponent<Button>();
         // | Listeners
         Button_Resume.onClick.AddListener(SwitchIsPaused);
-        Button_Play.onClick.AddListener(SwitchIsMenued);
-        Button_Menu.onClick.AddListener(SwitchIsMenued);
-        Button_Quit.onClick.AddListener(Application.Quit);
+        Button_Retry.onClick.AddListener(Play);
+        Button_Play.onClick.AddListener(Play);
+        Button_Menu.onClick.AddListener(Menu);
+        Button_Quit_P.onClick.AddListener(Application.Quit);
+        Button_Quit_M.onClick.AddListener(Application.Quit);
+        Button_Settings.onClick.AddListener(SwitchIsSettinged);
     }
     void Update()
     {
@@ -36,10 +43,13 @@ class UI : MonoBehaviour
         if (!isMenued && Input.GetKeyDown(KeyCode.Escape)) SwitchIsPaused();
         // UI Objects
         // | Layers
-        Layer_Runtime.SetActive(!isPaused && !isMenued);
-        Layer_Pause.SetActive(isPaused);
-        Layer_Menu.SetActive(isMenued);
+        Layer_Runtime.SetActive(!isPaused && !isMenued && !isSettinged);
+        Layer_Pause.SetActive(isPaused && !isSettinged);
+        Layer_Menu.SetActive(isMenued && !isSettinged);
+        Layer_Settings.SetActive(isSettinged);
     }
-    void SwitchIsPaused() { isPaused ^= true; }
-    void SwitchIsMenued() { isMenued ^= true; isPaused = false; }
+    void SwitchIsPaused() { isPaused ^= true; isSettinged = false; }
+    void Play() { SceneManager.LoadScene("Game"); }
+    void Menu() { SceneManager.LoadScene("Menu"); }
+    void SwitchIsSettinged() { isSettinged ^= true; }
 }
